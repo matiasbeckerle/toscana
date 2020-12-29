53f100bbe3a135ba44d91dd154360ead04ac6136
matiasbeckerle <matiasbeckerle@gmail.com>
2020-12-29 13:35:48 -0300

    Make RecipeContainer a function component

--- src/components/RecipeContainer.tsx
+++ src/components/RecipeContainer.tsx
@@ -1,27 +1,19 @@
-import React from 'react';
 import { Recipe } from './../models/recipe';
 
 type RecipeProps = {
   recipe: Recipe
 };
 
-type RecipeState = {};
+export const RecipeContainer = ({ recipe }: RecipeProps) =>
+  <dl>
+    <dt>Flour (100%)</dt>
+    <dd>1000<abbr title="grams">gr</abbr></dd>
+    <dt>Water ({recipe.water}%)</dt>
+    <dd>{calculateQuantity(recipe.water)}<abbr title="grams">gr</abbr></dd>
+    <dt>Salt ({recipe.salt}%)</dt>
+    <dd>{calculateQuantity(recipe.salt)}<abbr title="grams">gr</abbr></dd>
+  </dl>;
 
-export class RecipeContainer extends React.Component<RecipeProps, RecipeState> {
-  calculateQuantity = (percentage: number) => {
-    return (1000 * percentage) / 100;
-  };
-
-  render() {
-    return (
-      <dl>
-        <dt>Flour (100%)</dt>
-        <dd>1000<abbr title="grams">gr</abbr></dd>
-        <dt>Water ({this.props.recipe.water}%)</dt>
-        <dd>{this.calculateQuantity(this.props.recipe.water)}<abbr title="grams">gr</abbr></dd>
-        <dt>Salt ({this.props.recipe.salt}%)</dt>
-        <dd>{this.calculateQuantity(this.props.recipe.salt)}<abbr title="grams">gr</abbr></dd>
-      </dl>
-    );
-  }
+function calculateQuantity(percentage: number): number {
+  return (1000 * percentage) / 100;
 }
